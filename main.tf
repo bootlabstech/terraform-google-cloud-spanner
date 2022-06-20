852aebfa71da0704d2ec703506bb7d69d20b6bb8
Naveen Manokaran <naveen.manokaran@bootlabstech.com>
2022-06-20 18:39:16 +0530

    adding the .github folder

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-resource "google_spanner_instance" "example" {
-  name             = var.instance_id
-  project          = var.project_id 
-  config           = var.config
-  display_name     = var.instance_name
-  num_nodes        = var.num_nodes
-  force_destroy    = var.force_destroy
-  labels           = var.labels
+resource "google_spanner_instance" "instance" {
+  name          = var.instance_id
+  project       = var.project_id
+  config        = var.config
+  display_name  = var.instance_name
+  num_nodes     = var.num_nodes
+  force_destroy = var.force_destroy
+  labels        = length(keys(var.labels)) < 0 ? null : var.labels
 }
 
 resource "google_spanner_database" "database" {
   project             = var.project_id
-  instance            = google_spanner_instance.example.name
+  instance            = google_spanner_instance.instance.name
   name                = var.database_name
   deletion_protection = var.deletion_protection
-  
+
   encryption_config {
     kms_key_name = var.kms_key_name
   }
